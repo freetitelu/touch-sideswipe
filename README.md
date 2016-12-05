1d5c9b1ef8343748ad650a638bebe92a0ce26721
freetitelu <freetitelu@gmail.com>
2016-12-05 03:38:15 +0300

    0.1.1: add action-hamburger, license, min files, user-vars, comments, open/close func, actions and little-bit refactoring

--- README.md
+++ README.md
@@ -8,4 +8,32 @@
     <li>Activate "Toogle device toolbar"</li>
     <li>Touch (swipe) left window side to right</li>
 </ol>
-<h5>2016 MIT license. Author  Mititelu Nick (aka freetitelu)</h5>
+<i>//todo: create branch gh-page for demo
+</i>
+<br>
+<i>//todo: registrate in npm and other</i>
+<br>
+<i>//todo: think about right side swipe</i>
+<hr>
+
+<b>The MIT License (MIT)</b>
+
+<p>Copyright (c) 2016 Mititelu Nick (aka freetitelu)</p>
+
+<p>Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:</p>
+
+<p>The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.</p>
+
+<p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.</p>
--- src/touch-sideswipe.js
+++ src/touch-sideswipe.js
@@ -1,186 +1,195 @@
-/* touchSideSwipe v0.1.0
+/* touchSideSwipe v0.1.1
  * https://github.com/Lucyway/touch-sideswipe
- * MIT license 2016. Author Mititelu Nick (aka freetitelu)
+ * 2016 (c) Mititelu Nick (aka freetitelu). MIT license.
  */
-//todo: hamburger
-//todo: add in site
-//todo: create branch gh-page and gh-site for demo
-//todo: init and registrate in npm and bower
-
 (function(root, factory) {
     'use strict';
-    if (typeof define === 'function' && define.amd) {define(factory);}
-    else if (typeof module === 'object' && module.exports) {module.exports = factory();}
-    else {root.TouchSideSwipe = factory();}
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        root.TouchSideSwipe = factory();
+    }
 }(this, function() {
     var TouchSideSwipe = function(config) {
         'use strict';
-        //var TouchSideSwipe = TouchSideSwipe || function(config) {
-        var opt = {
-            wrappedElementID: config.wrappedElementID || 'touchSideSwipe',
-            sideHookWidth: config.sideHookWidth || 10,
+        var opt = { //default options
+            wrappedElementID: config.elementID || 'touchSideSwipe',
+            wrappedElementWidth: config.elementWidth || 400, //px
+            wrappedElementMaxWidth: config.elementMaxWidth || 0.8, // *100%
+            sideHookWidth: config.sideHookWidth || 10, //px
+            moveSpeed: config.moveSpeed || 0.2, //sec
+            opacityBackground: config.opacityBackground || 0.8,
         };
-        //var tssElemCoordX2;
         var touchmoveCoordXTmp = [];
         var touchstartCoordX;
         var open = false;
         var screenAvailWidth = window.screen.availWidth;
-        var tssElemCoordX1;
-
+        var tssElemCoordX0;
         var tssElem = document.createElement('div');
         var wrappedElem = document.getElementById(opt.wrappedElementID);
 
-        //wrap elem in touchSideSwipe DOM element
+        //------------------------------------------------------------------
+        // wrap elem in touchSideSwipe DOM element
+        //------------------------------------------------------------------
         wrappedElem.parentNode.insertBefore(tssElem, wrappedElem);
         tssElem.appendChild(wrappedElem);
 
-        //define 'width' and display hidden inner DOM-element
+        //------------------------------------------------------------------
+        // create first parameters: width and state wrapped DOM-element
+        //------------------------------------------------------------------
         wrappedElem.classList.add('tss_wrapped');
         var wrappedElemWidth;
-        if(screenAvailWidth>499){
-            wrappedElemWidth = wrappedElem.clientWidth;
-        }
-        else {
+        if (screenAvailWidth > 499) {
+            wrappedElemWidth = opt.wrappedElementWidth;
+        } else {
             wrappedElemWidth = screenAvailWidth * 0.8;
         }
         wrappedElem.style.width = wrappedElemWidth + 'px';
         var tssElemWidth = wrappedElemWidth + opt.sideHookWidth;
-        //
         tssElem.classList = 'tss tss--close';
         tssElem.style.transform = 'translateX(' + (-wrappedElemWidth) + 'px)';
         tssElem.style.width = tssElemWidth + 'px';
 
-        window.onresize = function() {
+        //------------------------------------------------------------------
+        // recalc parameters on resize window
+        //------------------------------------------------------------------
+        function tssWinOnresize() {
             if (open === true) {
                 tssClose(tssElem, bgElem);
             }
             screenAvailWidth = window.screen.availWidth;
-            if (screenAvailWidth<500){
+            if (screenAvailWidth < 500) {
                 wrappedElemWidth = screenAvailWidth * 0.8;
-            }
-            else{
-                wrappedElemWidth = wrappedElem.clientWidth;
+            } else {
+                wrappedElemWidth = opt.wrappedElementWidth;
             }
             tssElemWidth = wrappedElemWidth + opt.sideHookWidth;
             wrappedElem.style.width = wrappedElemWidth + 'px';
             tssElem.style.transform = 'translateX(' + (-wrappedElemWidth) + 'px)';
             tssElem.style.width = tssElemWidth + 'px';
-            console.log(wrappedElemWidth + ' | ' + tssElemWidth);
-        };
+        }
 
-        //background DOM element
+        //------------------------------------------------------------------
+        // create background DOM element
+        //------------------------------------------------------------------
         var bgElem = document.createElement('div');
         document.body.insertBefore(bgElem, document.body.lastChild);
-        bgElem.classList = 'tss_bg';
+        bgElem.classList = 'tss-bg';
+
+        //------------------------------------------------------------------
+        // create label DOM element
+        //------------------------------------------------------------------
+        var labelElem = document.createElement('div');
+        wrappedElem.appendChild(labelElem);
+        labelElem.classList = 'tss-label';
 
         //------------------------------------------------------------------
         // start touch-event
         //------------------------------------------------------------------
-        tssElem.addEventListener(
-            'touchstart',
-            function(event) {
-                /*wrappedElemWidth = wrappedElem.clientWidth;
-                tssElemWidth = wrappedElemWidth + opt.sideHookWidth;
-                //if (open === false) {
-                    tssElem.style.width = tssElemWidth + 'px';
-                //}*/
+        function tssTouchstart(event) {
+            tssElem.style.transitionDuration = '0s';
+            bgElem.style.transitionDuration = '0s';
+            bgElem.style.zIndex = 999;
+            tssElemCoordX0 = tssElem.getBoundingClientRect().left;
+            touchstartCoordX = event.touches[0].pageX;
+        }
 
-                tssElem.classList.remove('tss--transition');
-                //elem.classList.remove('transition');
-                bgElem.classList.remove('tss--transition');
-                bgElem.style.zIndex = 999;
-
-                var tssElemCoord = tssElem.getBoundingClientRect();
-                //tssElemCoordX1 = box.left + pageXOffset;
-                tssElemCoordX1 = tssElemCoord.left;
-                touchstartCoordX = event.touches[0].pageX;
-            },
-            false
-        );
         //------------------------------------------------------------------
         // Drag element
         //------------------------------------------------------------------
-        tssElem.addEventListener(
-            'touchmove',
-            function(event) {
-                event.preventDefault();
-
-                var touchMoveCoordX = event.touches[0].pageX;
-                var touchmoveCoordX1 = touchMoveCoordX - (touchstartCoordX - tssElemCoordX1);
-                if (touchmoveCoordX1 < 0) {
-                    tssElem.style.transform = 'translateX(' + touchmoveCoordX1 + 'px)';
-                    var bgElemOpacity = touchMoveCoordX / wrappedElemWidth;
-                    if (bgElemOpacity > 0 && bgElemOpacity < 1) {
-                        if (bgElemOpacity >= 0.8) {
-                            bgElem.style.opacity = 0.8
-                        } else {
-                            bgElem.style.opacity = bgElemOpacity
-                        }
-                    };
-                    if (touchMoveCoordX > wrappedElemWidth) {
-                        wrappedElem.style.transform = 'translateX(' + (-touchmoveCoordX1) + 'px)';
-                        bgElem.style.opacity = 0.8;
+        function tssTouchmove(event) {
+            event.preventDefault();
+            var touchmoveCoordX = event.touches[0].pageX;
+            var tssElemCoordX0New = touchmoveCoordX - (touchstartCoordX - tssElemCoordX0);
+            if (tssElemCoordX0New < 0) {
+                tssElem.style.transform = 'translateX(' + tssElemCoordX0New + 'px)';
+                var bgElemOpacity = touchmoveCoordX / wrappedElemWidth;
+                if (bgElemOpacity > 0 && bgElemOpacity < 1) {
+                    if (bgElemOpacity >= opt.opacityBackground) {
+                        bgElem.style.opacity = opt.opacityBackground;
+                    } else {
+                        bgElem.style.opacity = bgElemOpacity;
                     }
                 }
-                //remember swipe vector
-                touchmoveCoordXTmp.push(event.touches[0].pageX);
-            },
-            false
-        );
+                if (touchmoveCoordX > wrappedElemWidth) {
+                    wrappedElem.style.transform = 'translateX(' + (-tssElemCoordX0New) + 'px)';
+                    bgElem.style.opacity = opt.opacityBackground;
+                }
+            }
+            //remember swipe vector
+            touchmoveCoordXTmp.push(event.touches[0].pageX);
+        }
+
         //------------------------------------------------------------------
         // end touch-event
         //------------------------------------------------------------------
-        function tssOpen(tssElem, bgElem) {
-            open = true;
+        function tssTouchend(event) {
+            tssElem.style.transitionDuration = opt.moveSpeed + 's';
+            bgElem.style.transitionDuration = opt.moveSpeed + 's';
+            wrappedElem.style.transform = '';
+            var touchendXLast = touchmoveCoordXTmp[touchmoveCoordXTmp.length - 1];
+            var touchendXBeforeLast = touchmoveCoordXTmp[touchmoveCoordXTmp.length - 2];
+            if (Math.abs(touchendXLast - touchendXBeforeLast) > 0) {
+                if (touchendXLast > touchendXBeforeLast) {
+                    tssOpen(tssElem, bgElem);
+                } else {
+                    tssClose(tssElem, bgElem);
+                }
+            }
+            touchmoveCoordXTmp = [];
+        }
+
+        //------------------------------------------------------------------
+        // open/close on click
+        //------------------------------------------------------------------
+        function tssClick(event) {
+            if (open === false) {
+                tssOpen();
+            } else {
+                //click between the open element and the right side of the window
+                if (event.clientX > wrappedElemWidth) {
+                    tssClose();
+                }
+            }
+        }
+
+        //------------------------------------------------------------------
+        // open/close functions
+        //------------------------------------------------------------------
+        function tssOpen() {
+            bgElem.style.opacity = opt.opacityBackground;
+            tssElem.style.width = screenAvailWidth + 'px';
+            tssElem.style.transform = 'translateX(0px)';
             tssElem.classList.remove('tss--close');
             tssElem.classList.add('tss--open');
-            bgElem.classList.remove('tss_bg--close');
-            bgElem.classList.add('tss_bg--open');
+            bgElem.classList.remove('tss-bg--close');
+            bgElem.classList.add('tss-bg--open');
+            bgElem.style.zIndex = '999';
+            open = true;
         }
 
-        function tssClose(tssElem, bgElem) {
-            open = false;
+        function tssClose() {
+            bgElem.style.opacity = 0;
+            tssElem.style.width = tssElemWidth + 'px';
+            tssElem.style.transform = 'translateX(' + (-wrappedElemWidth) + 'px)';
             tssElem.classList.remove('tss--open');
             tssElem.classList.add('tss--close');
-            bgElem.classList.remove('tss_bg--open');
-            bgElem.classList.add('tss_bg--close');
+            bgElem.classList.remove('tss-bg--open');
+            bgElem.classList.add('tss-bg--close');
+            bgElem.style.zIndex = '-999';
+            open = false;
         }
-        /*tssElem.addEventListener(
-            'touchend',
-            function end(event) {
 
-            },
-            false
-        );*/
-        tssElem.addEventListener(
-            'touchend',
-            function(event) {
-                
-                tssElem.classList.add('tss--transition');
-
-                //elem.classList.add('transition');
-                wrappedElem.style.transform = '';
-                //elem.classList.add('transition');
-                bgElem.style.opacity = '';
-                bgElem.classList.add('tss--transition');
-                bgElem.style.zIndex = '';
-                var touchendX = touchmoveCoordXTmp[touchmoveCoordXTmp.length - 1];
-                var touchendXBefore = touchmoveCoordXTmp[touchmoveCoordXTmp.length - 2];
-                if (Math.abs(touchendX - touchendXBefore) > 0) {
-                    if (touchendX > touchendXBefore) {
-                        tssOpen(tssElem, bgElem);
-                        tssElem.style.width = screenAvailWidth + 'px';
-                        tssElem.style.transform = 'translateX(0px)';
-                    } else {
-                        tssClose(tssElem, bgElem);
-                        tssElem.style.width = tssElemWidth + 'px';
-                        tssElem.style.transform = 'translateX(' + (-wrappedElemWidth) + 'px)';
-                    }
-                }
-                touchmoveCoordXTmp = [];
-            },
-            false
-        );
+        //------------------------------------------------------------------
+        // actions
+        //------------------------------------------------------------------
+        window.onresize = tssWinOnresize;
+        tssElem.addEventListener('touchstart', tssTouchstart, false);
+        tssElem.addEventListener('touchmove', tssTouchmove, false);
+        tssElem.addEventListener('touchend', tssTouchend, false);
+        tssElem.addEventListener('click', tssClick, false);
     };
     return TouchSideSwipe;
 }));
